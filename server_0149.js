7b7363e2f134d86c0ca0ffdc42026effafbf369b
Nawaporn Viriyaparp <totoiuy@gmail.com>
2021-09-01 15:23:19 +0000

    fix

--- server_0149.js
+++ server_0149.js
@@ -9,7 +9,8 @@ const express = require('express');
 // App
   const app = express();
   app.get('/', (req, res) => {
-   res.send('Hello, Wrld');
+   res.send('Hello, World');
+
    });
 
    app.listen(PORT, HOST);
